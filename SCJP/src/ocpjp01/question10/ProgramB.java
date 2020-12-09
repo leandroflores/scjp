d67b7391680fc79e53e109e92c0cc054e24c616f
leandroflores <leandroflores7@gmail.com>
2020-12-09 13:24:14 -0200

    - Removida Questões da OCJP.

--- SCJP/src/ocpjp01/question10/ProgramB.java
+++
@@ -1,9 +0,0 @@
-package ocpjp01.question10;
-
-public class ProgramB {
-    public static void main(String[] args) {
-        java.util.ArrayList<String> list = new java.util.ArrayList<String>();
-        list.add("element 1");
-        System.out.println("An element is added to the array list.");
-    }
-}
